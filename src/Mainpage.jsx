a7264ef09f8f07408c7fa45ca24a43868770d34a
kupelaphiri <kupelaphiri2003@gmail.com>
2024-01-11 01:32:08 +0200

    chore:notes displayed fro database

--- src/Mainpage.jsx
+++ src/Mainpage.jsx
@@ -1,68 +1,13 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useUtilities } from './hooks/useOutsideClickDetector';
 
-const pinnedNotes = [
-  {
-    title: 'Yt ideas',
-    content:
-      '-How to not be a simp how to get girls-delete world -how to be cool -youtube tutorials be like -gangsta with feminine names -how to clap without your hands.',
-  },
-  {
-    title: '',
-    content:
-      'Cooking oil K38 Washing soap K30 Onion K10 Bread K15 Noodles K30 Sausage K50.',
-  },
-  {
-    title: 'Addma question',
-    content: 'Addma paper 2 2015 question 13 Addma 2016 question 12.',
-  },
-  {
-    title: '',
-    content: 'Lexus IS 250.',
-  },
-  {
-    title: 'Rush hour 3.',
-    content: '',
-  }, 
-  {
-    title: 'Rush hour 3.',
-    content: '',
-  },
-  
-];
-
-const otherNotes = [
-  {
-    title: 'Yt ideas',
-    content:
-      '-How to not be a simp how to get girls-delete world -how to be cool -youtube tutorials be like -gangsta with feminine names -how to clap without your hands.',
-  },  
-   {
-    title: '',
-    content:
-      'Cooking oil K38 Washing soap K30 Onion K10 Bread K15 Noodles K30 Sausage K50.',
-  },
-  {
-    title: 'Addma question',
-    content: 'Addma paper 2 2015 question 13 Addma 2016 question 12.',
-  },
-  {
-    title: '',
-    content: 'Lexus IS 250.',
-  },
-  {
-    title: 'Rush hour 3.',
-    content: '',
-  }, 
-  {
-    title: 'Rush hour 3.',
-    content: '',
-  },
-]
 
 function Mainpage() {
   const [isActive, setIsActive] = useState(false);
+  const [backendPinnedData, setBackendPinnedData] = useState([{}])
+  const [backendOtherData, setBackendOtherData] = useState([{}])
+  
   const InputRef = useRef(null);
   const { useOutsideClickDetector } = useUtilities();
  
@@ -75,6 +20,16 @@ function Mainpage() {
   };
 
   useOutsideClickDetector(InputRef, unfocus);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/all-notes').then(res => res.json())
+    .then(notes => setBackendPinnedData(notes))
+  }, [])
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/other').then(res => res.json())
+    .then(data => setBackendOtherData(data))
+  })
   
 
   return (
@@ -102,12 +57,12 @@ function Mainpage() {
       <div className="flex flex-col w-full pr-2 h-full">
         <p className='text-xs font-bold pl-[20px]'>PINNED</p>
         <div className="flex flex-row flex-wrap h-[500px] pl-[15px] overflow-hidden items-baseline pr-[5px]">
-          {pinnedNotes.map((note) => {
+          {backendPinnedData.map((note) => {
             return (
              
-                <div key={note.title} className="w-[280px] min-h-24 max-h-[452px] overflow-hidden border-[1px] mt-[20px] p-5 rounded-lg mr-4">
+                <div key={note.id} className="w-[280px] min-h-24 max-h-[452px] overflow-hidden border-[1px] mt-[20px] p-5 rounded-lg mr-4">
                   <h2>{note.title}</h2>
-                  <p>{note.content}</p>
+                  <p>{note.body}</p>
                 </div>
              
             );
@@ -118,7 +73,7 @@ function Mainpage() {
        <p className='text-xs font-bold pl-[20px]'>OTHERS</p>
 
         <div className='flex flex-row h-[500px] pl-[15px] overflow-hidden items-baseline pr-[5px]'>
-       {otherNotes.map((note) => {
+       {backendOtherData.map((note) => {
         return (
            <div key={note.title} className="w-[280px] min-h-24 max-h-[452px] overflow-hidden border-[1px] mt-[20px] p-5 rounded-lg mr-4">
             <h2>{note.title}</h2>
@@ -127,34 +82,7 @@ function Mainpage() {
         );
        })}
        </div>
-        {/* <div className='flex flex-col w-full mt-40'>
-      <div className=' w-100px h-100px ml-20'>
-       <p>OTHERS</p>
-       </div>
-       <div className='flex flex-row '>
-       <div className='w-1/4 h-100px border-2 mt-5 ml-20 p-5 rounded-lg'>
-        <h2>YT video ideas</h2>
-        <p>Body shamming <br />
-What I fear about marriage. <br />
-Not everyone can be included in everything. <br /> 
-NBA youngboy is trash</p>
-       </div>
-       <div className='w-1/4 h-100px border-2 mt-5 ml-5 p-5 rounded-lg'>
-        <h2>YT video ideas</h2>
-        <p>Body shamming <br />
-What I fear about marriage. <br />
-Not everyone can be included in everything. <br /> 
-NBA youngboy is trash</p>
-       </div>
-       <div className='w-1/4 h-100px border-2 mt-5 ml-5 p-5 rounded-lg'>
-        <h2>YT video ideas</h2>
-        <p>Body shamming <br />
-What I fear about marriage. <br />
-Not everyone can be included in everything. <br /> 
-NBA youngboy is trash</p>
-       </div>
-     </div>
-   </div> */}
+       
       </div>
     </div>
   );
