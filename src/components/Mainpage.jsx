8b9b644dff59c5fe6eb00b68698758de656fa51f
kupelaphiri <kupelaphiri2003@gmail.com>
2024-03-19 23:53:33 +0200

    feat:empty state added

--- src/components/Mainpage.jsx
+++ src/components/Mainpage.jsx
@@ -227,7 +227,7 @@ function Mainpage() {
     <>
  
     <div
-        className={`h-screen flex flex-col w-full min-h-full ${isLoading? 'justify-center items-center' : ''} overflow-y-auto pb-[100px] flex-1 shrink-0`}
+        className={`h-screen flex flex-col w-full min-h-full ${isLoading? 'justify-center items-center' : ''} ${backendPinnedData == null ? 'overflow-y-hidden' : ''} overflow-y-auto pb-[100px] flex-1 shrink-0`}
       >
        {isLoading?(
       <l-ring
@@ -277,11 +277,14 @@ function Mainpage() {
         </div>
      
         
-      <div>
+    <div className={`h-screen flex flex-col w-full min-h-full ${backendPinnedData == null ? 'pt-[70px] items-center' : ''} overflow-y-auto pb-[100px] flex-1 shrink-0`}>
+
     {backendPinnedData == null ? (
-      <svg xmlns="http://www.w3.org/2000/svg" className='left-[25px] w-[25px] text-gray-500' fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 0 0 1.5-.189m-1.5.189a6.01 6.01 0 0 1-1.5-.189m3.75 7.478a12.06 12.06 0 0 1-4.5 0m3.75 2.383a14.406 14.406 0 0 1-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 1 0-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
-     </svg>
+      
+       <svg xmlns="http://www.w3.org/2000/svg" className='left-[25px] w-[200px] text-gray-300' fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 0 0 1.5-.189m-1.5.189a6.01 6.01 0 0 1-1.5-.189m3.75 7.478a12.06 12.06 0 0 1-4.5 0m3.75 2.383a14.406 14.406 0 0 1-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 1 0-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
+       </svg>
+     
     ) : (
       <div
         className={`h-screen flex flex-col w-full min-h-full  overflow-y-auto pb-[100px] flex-1 shrink-0`}
@@ -498,7 +501,8 @@ function Mainpage() {
         
       </div>
     )}
-     </div>
+    </div>
+    
       </div>
       )}
 
